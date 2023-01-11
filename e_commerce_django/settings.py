96a915b2c7c3d0fcd08272026cdd73e3bec68035
JoKeR-VIKING <prathamvasani1@gmail.com>
2023-01-11 14:34:36 +0530

    allowed host

--- e_commerce_django/settings.py
+++ e_commerce_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1z0!2dc80a%=&j3&zl=v5p!&z!2lw^est^&ylx05voxml9xx4*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
